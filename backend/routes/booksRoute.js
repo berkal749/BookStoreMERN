b4af2726af4266a07d8902588ebe2d3e25ae5208
berkal749 <berkalachrefpro@gmail.com>
2025-10-24 14:53:46 +0100

    the delete option added

--- backend/routes/booksRoute.js
+++ backend/routes/booksRoute.js
@@ -129,7 +129,7 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-router.delete("/books/:id", async (req, res) => {
+router.delete("/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const result = await Book.findByIdAndDelete(id);
